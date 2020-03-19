eb1fe27b72bfb878d82b676f5c887dca2fde7b39
Shabalin Slava <svaznoy0309@gmail.com>
2020-03-19 17:49:38 +0800

    Preloader

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Header from "./components/Header/Header";
-import Navigation from "./components/Navigation/Navigation";
-import Profile from "./components/Profile/Profile";
-import Dialogs from "./components/Dialogs/Dialogs";
-import { BrowserRouter, Route } from "react-router-dom";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import UsersContainer from "./components/Users/UsersContainer";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header/Header';
+import Navigation from './components/Navigation/Navigation';
+import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
+import { BrowserRouter, Route } from 'react-router-dom';
+import DialogsContainer from './components/Dialogs/DialogsContainer';
+import UsersContainer from './components/Users/UsersContainer';
 
 function App(props) {
   return (
     <BrowserRouter>
-      <div className="app-wrapper">
+      <div className='app-wrapper'>
         <Header />
-        <div className="wrap">
+        <div className='wrap'>
           <Navigation />
-          <Route  path="/profile" render={ () => <Profile /> } />
-          <Route  path="/dialogs" render={ () => <DialogsContainer /> } />
-          <Route  path="/users" render={ () => <UsersContainer /> } />
+          <Route  path='/profile' render={ () => <Profile /> } />
+          <Route  path='/dialogs' render={ () => <DialogsContainer /> } />
+          <Route  path='/users' render={ () => <UsersContainer /> } />
         </div>
       </div>
     </BrowserRouter>
--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -1,39 +1,50 @@
 import React from 'react';
 import {connect} from "react-redux";
-import {followAC, setUsersAC, unfollowAC, setCurrentPageAC, setUsersTotalCountAC} from "../../redux/users-reducer";
+import {followAC, setUsersAC, unfollowAC, setCurrentPageAC, setUsersTotalCountAC, toggleIsFetchingAC} from '../../redux/users-reducer';
 import * as axios from 'axios';
 import Users from './Users';
+import Preloader from '../../components/common/preloader/preloader';
 class UsersContainer extends React.Component {
   componentDidMount() {
+    this.props.toggleIsFetching(true);
     axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
-      this.props.setUsers(response.data.items)
-      this.props.setTotalUsersCount(response.data.totalCount)
+      this.props.toggleIsFetching(false);
+      this.props.setUsers(response.data.items);
+      this.props.setTotalUsersCount(response.data.totalCount);
     });
   }
   onPageChanged = (pageNumber) => {
      this.props.setCurrentPage(pageNumber);
+     this.props.toggleIsFetching(true);
      axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+       this.props.toggleIsFetching(false);
        this.props.setUsers(response.data.items)
      });
   }
   render() {
-    return <Users totalUsersCount={this.props.totalUsersCount}
+    return < >
+    {this.props.isFetching ? <Preloader /> : null}
+
+     <Users totalUsersCount={this.props.totalUsersCount}
       pageSize={this.props.pageSize}
       currentPage={this.props.currentPage}
       onPageChanged={this.onPageChanged}
       users={this.props.users}
       follow={this.props.follow}
-      unfollow={this.props.unfollow} />
+      unfollow={this.props.unfollow}
+      />
+    </>
 
   }
 }
 
 let mapStateToProps = (state) => {
-  return {
+  return  {
      users: state.usersPage.users,
      pageSize: state.usersPage.pageSize,
      totalUsersCount: state.usersPage.totalUsersCount,
      currentPage: state.usersPage.currentPage,
+     isFetching: state.usersPage.isFetching
   }
 }
 let mapDispatchToProps = (dispatch) => {
@@ -52,6 +63,9 @@ let mapDispatchToProps = (dispatch) => {
     },
     setTotalUsersCount: (totalCount) => {
       dispatch(setUsersTotalCountAC(totalCount))
+    },
+    toggleIsFetching:(isFetching) => {
+      dispatch(toggleIsFetchingAC(isFetching))
     }
   }
 }
